4552bd9a0d834af5ba0485b34df3b50c1bae391e
KingaW20 <kingawidla12@wp.pl>
2021-05-07 18:41:39 +0200

    Substract functionality ready

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,9 +4,43 @@ namespace Calculator
 {
     class Program
     {
+        public enum OptionType
+        {
+            None = 0,
+            Substract = 1
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var option = DisplayMenuForUser();
+
+            switch (option)
+            {
+                case OptionType.Substract:
+                    Substract();
+                    break;
+                case OptionType.None:
+                    return;
+            }
+
+            Console.Read();
+        }
+
+        private static OptionType DisplayMenuForUser()
+        {
+            Console.WriteLine("Choose option to do some crazy stuff:");
+            Console.WriteLine("1. Substract");
+
+            var option = Console.ReadLine();
+            return (OptionType)Enum.Parse(typeof(OptionType), option);
+        }
+
+        private static void Substract()
+        {
+            Console.WriteLine("Give me 2 numbers:");
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Your result is {x - y}.");
         }
     }
 }
